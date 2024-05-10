0d722e58a9b9220ce2110b1695a8e5a9d90dc60e
Julian Declercq <julian.declercq1@gmail.com>
2024-05-10 22:01:18 +0200

    remove unused builder stuff

--- MagickSandbox/MagickSandbox/Program.cs
+++ MagickSandbox/MagickSandbox/Program.cs
@@ -1,9 +1,4 @@
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.WebHost.UseUrls("http://0.0.0.0:8080");
-builder.Services.AddControllers();
-
 var app = builder.Build();
 app.MapGet("/", () => "Hello there");
 app.Run();
